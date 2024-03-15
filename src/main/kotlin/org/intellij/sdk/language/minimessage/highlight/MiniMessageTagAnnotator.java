214416b933d8659b979637a44188f998ec5d6b93
CubBossa <leonard.bausenwein@web.de>
2024-03-15 08:58:30 +0100

    closes #7 tag annotation fix

--- src/main/kotlin/org/intellij/sdk/language/minimessage/highlight/MiniMessageTagAnnotator.java
+++ src/main/kotlin/org/intellij/sdk/language/minimessage/highlight/MiniMessageTagAnnotator.java
@@ -24,74 +24,94 @@ public class MiniMessageTagAnnotator implements Annotator, DumbAware {
             return;
         }
         if (element.getContainingFile().getLanguage() instanceof MiniMessageLanguage mml) {
+            PsiElement[] els = new PsiElement[tag.getAttributes().length + 1];
+            els[0] = tag.getChildren()[1];
+            for (int i = 1; i <= tag.getAttributes().length; i++) {
+                els[i] = tag.getAttributes()[i - 1].getValueElement();
+            }
             for (MiniMessageTag mmTag : mml.getTags()) {
                 if (!mmTag.check(tag.getName())) {
                     continue;
                 }
 
-                PsiElement[] els = new PsiElement[tag.getAttributes().length + 1];
-                els[0] = tag.getChildren()[1];
-                for (int i = 1; i <= tag.getAttributes().length; i++) {
-                    els[i] = tag.getAttributes()[i - 1].getValueElement();
-                }
-
-                annotateRecursively(tag, annotationHolder, els, mmTag, 1).getValue().run();
+                new MiniMessageTagAnnotationProcessor(tag, annotationHolder).annotate(els, mmTag, 1).getValue().run();
             }
         }
     }
 
-    private Map.Entry<Integer, Runnable> annotateRecursively(XmlTag tag, AnnotationHolder holder, PsiElement[] attributes, Argument argument, int level) {
-        if (attributes.length <= level) {
+    private static class MiniMessageTagAnnotationProcessor {
+
+        private final XmlTag tag;
+        private final AnnotationHolder annotationHolder;
+
+        public MiniMessageTagAnnotationProcessor(XmlTag tag, AnnotationHolder annotationHolder) {
+            this.tag = tag;
+            this.annotationHolder = annotationHolder;
+        }
+
+        private Map.Entry<Integer, Runnable> annotate(PsiElement[] attributes, Argument argument, int level) {
+            // There is no PsiElement to match any child of argument
+            if (attributes.length <= level) {
+                // If argument has no children it's fine, we don't complain
+                if (argument.getChildren().isEmpty()) {
+                    return Map.entry(1, () -> {});
+                }
+                // If none of the children is mandatory it's fine.
+                if (argument.getChildren().stream().anyMatch(Argument::isOptional)) {
+                    return Map.entry(1, () -> {});
+                }
+                // Otherwise we complain that we miss an attribute. We complain on the last possible PsiElement
+                return Map.entry(1, () -> {
+                    annotationHolder.newAnnotation(HighlightSeverity.ERROR, "Required argument missing: " + argument.getChildren().stream()
+                                    .filter(a -> !a.isOptional()).map(Argument::getName)
+                                    .collect(Collectors.joining("|")))
+                            .range(attributes.length == 0 ? tag.getChildren()[1].getTextRange() : attributes[attributes.length - 1].getTextRange())
+                            .create();
+                });
+            }
+            // The provided argument has no children, so nothing to check
             if (argument.getChildren().isEmpty()) {
-                return Map.entry(level, () -> {});
-            }
-            if (argument.getChildren().stream().anyMatch(Argument::isOptional)) {
-                return Map.entry(level, () -> {});
-            }
-            return Map.entry(level, () -> {
-                holder.newAnnotation(HighlightSeverity.ERROR, "Required argument missing: " + argument.getChildren().stream()
-                                .filter(a -> !a.isOptional()).map(Argument::getName)
-                                .collect(Collectors.joining("|")))
-                        .range(attributes.length == 0 ? tag.getChildren()[1].getTextRange() : attributes[attributes.length - 1].getTextRange())
-                        .create();
-            });
-        }
-        if (argument.getChildren().isEmpty()) {
-            var attr = attributes[level];
-            return Map.entry(level, () -> {
-                holder.newAnnotation(HighlightSeverity.WARNING, "unexpected argument '" + attr.getText() + "'.")
-                        .range(attr.getTextRange())
-                        // Remove one from level because the tag itself is not an attribute but included in level count
-                        .withFix(new MiniMessageRemoveUnexpectedArgumentsFix(tag, level - 1))
-                        .create();
-            });
-        }
-
-        String text = StringUtil.unquote(attributes[level].getText());
-        int l = 0;
-        Argument chosenOne = null;
-        Runnable annotateLater = () -> {};
-        for (Argument child : argument.getChildren()) {
-            if (!child.check(text)) {
-                continue;
+                var attr = attributes[level];
+                return Map.entry(1, () -> {
+                    annotationHolder.newAnnotation(HighlightSeverity.WARNING, "unexpected argument '" + attr.getText() + "'.")
+                            .range(attr.getTextRange())
+                            // Remove one from level because the tag itself is not an attribute but included in level count
+                            .withFix(new MiniMessageRemoveUnexpectedArgumentsFix(tag, level - 1))
+                            .create();
+                });
             }
 
-            var e = annotateRecursively(tag, holder, attributes, child, level + 1);
-            if (e.getKey() > l) {
-                chosenOne = child;
-                annotateLater = e.getValue();
-                l = e.getKey();
+            // Validate the PsiElement value
+            String text = StringUtil.unquote(attributes[level].getText());
+            int successfulArgumentLength = 0;
+            Argument chosenOne = null;
+            Runnable annotateLater = () -> {};
+            // Pick the child argument that matches with the most possible length
+            for (Argument child : argument.getChildren()) {
+                if (!child.check(text)) {
+                    continue;
+                }
+
+                var e = annotate(attributes, child, level + 1);
+                if (e.getKey() > successfulArgumentLength) {
+                    chosenOne = child;
+                    annotateLater = e.getValue();
+                    successfulArgumentLength = e.getKey();
+                }
             }
-        }
-        if (chosenOne == null) {
-            PsiElement attr = attributes[level];
-            return Map.entry(l, () -> {
-                holder.newAnnotation(HighlightSeverity.WARNING, "One of the following values expected: " + argument.getChildren().stream().map(Argument::getName).collect(Collectors.joining("|")))
-                        .range(attr.getTextRange())
-                        .create();
-            });
-        } else {
-            return Map.entry(l, annotateLater);
+
+            // No child argument matched, so we complain
+            if (chosenOne == null) {
+                PsiElement attr = attributes[level];
+                return Map.entry(successfulArgumentLength + 1, () -> {
+                    annotationHolder.newAnnotation(HighlightSeverity.WARNING, "One of the following values expected: " + argument.getChildren().stream().map(Argument::getName).collect(Collectors.joining("|")))
+                            .range(attr.getTextRange())
+                            .create();
+                });
+            } else {
+                // + 1 because it was success on this argument
+                return Map.entry(successfulArgumentLength + 1, annotateLater);
+            }
         }
     }
 }
